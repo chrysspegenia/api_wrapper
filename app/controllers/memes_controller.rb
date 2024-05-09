b5244c6d2479f69b62cd1c58d87e9cbfaf649db2
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-05-09 20:43:20 +0800

    Create client method

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,18 +1,21 @@
 class MemesController < ApplicationController
   
   def index
-    client = Memegen::V1::Client.new
     @memes = client.meme_list
   end
 
   def generate_meme
       begin
-        client = Memegen::V1::Client.new
         @meme= client.meme(meme: "10-Guy", top: "me", bottom: "after 500hrs of trial and error")
       rescue Memegen::V1::Client::MemeNotFound
         flash[:error] = "Meme not found. Please try again later."
       end
   end
 
+  private
+
+  def client
+    Memegen::V1::Client.new
+  end
 end
   
